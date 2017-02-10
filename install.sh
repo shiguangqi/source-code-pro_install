6775886c144e623e5b47b60d7bdd4d0433958dfa
shiguangqi <shiguangqi2008@gmail.com>
2017-02-10 17:31:40 +0800

    Update install.sh
    修改下载地址

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir /tmp/adodefont
 cd /tmp/adodefont
-wget http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip
-unzip SourceCodePro_FontsOnly-1.017.zip
+wget https://github.com/downloads/adobe-fonts/source-code-pro/SourceCodePro_FontsOnly-1.013.zip
+unzip SourceCodePro_FontsOnly-1.013.zip
 mkdir -p /usr/local/share/fonts/
-cp SourceCodePro_FontsOnly-1.017/OTF/*.otf /usr/local/share/fonts/
+cp SourceCodePro_FontsOnly-1.013/OTF/*.otf /usr/local/share/fonts/
 fc-cache -f -v
